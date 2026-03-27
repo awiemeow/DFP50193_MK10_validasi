ca85472b12f10499e9682e56a345897c3468e9c7
catwi <meowawie@gmail.com>
2026-03-27 16:40:18 +0800

    Make form fields required for submission
    Added 'required' attribute to input fields for validation.

--- validasi/borang.php
+++ validasi/borang.php
@@ -13,19 +13,19 @@
 <form class="form" action="proses.php" method="POST">
 
 <label class="label">Nama Pelajar</label>
-<input type="text" name="nama" class="input">
+<input type="text" name="nama" class="input" required>
 
 <label class="label">No. Matrik</label>
-<input type="text" name="matrik" class="input">
+<input type="text" name="matrik" class="input" required>
 
 <label class="label">Umur</label>
-<input type="number" name="umur" class="input">
+<input type="number" name="umur" class="input" required>
 
 <label class="label">Tarikh Permohonan</label>
-<input type="date" name="tarikh" class="input">
+<input type="date" name="tarikh" class="input" required>
 
 <label class="label">Program Pengajian</label>
-<select name="program" class="input">
+<select name="program" class="input" required>
     <option value="">-- Pilih Program --</option>
     <option value="Diploma IT">Diploma IT</option>
     <option value="Diploma CS">Diploma Computer Science</option>
@@ -33,16 +33,16 @@
 </select>
 
 <label class="label">Jenis Komputer</label><br>
+<input type="radio" name="jenis" value="Laptop"> Laptop
+<input type="radio" name="jenis" value="Desktop"> Desktop
 
-<input type="radio" name="jenis" value="Laptop" class="radio"> Laptop
-<input type="radio" name="jenis" value="Desktop" class="radio"> Desktop
 <br><br>
 
 <label class="label">Spesifikasi Diperlukan</label><br>
+<input type="checkbox" name="spec[]" value="RAM 8GB"> RAM 8GB
+<input type="checkbox" name="spec[]" value="SSD 512GB"> SSD 512GB
+<input type="checkbox" name="spec[]" value="Graphic Card"> Graphic Card
 
-<input type="checkbox" name="spec[]" value="RAM 8GB" class="checkbox"> RAM 8GB
-<input type="checkbox" name="spec[]" value="SSD 512GB" class="checkbox"> SSD 512GB
-<input type="checkbox" name="spec[]" value="Graphic Card" class="checkbox"> Graphic Card
 <br><br>
 
 <label class="label">Alasan Permohonan</label>
@@ -50,11 +50,10 @@
 
 <br><br>
 
-<button type="submit" name="hantar" class="buttonSubmit">Hantar</button>
-<button type="reset" class="buttonReset">Tetap Semula</button>
+<button type="submit" name="hantar">Hantar</button>
+<button type="reset">Tetap Semula</button>
 
 </form>
-
 </div>
 
 </body>
